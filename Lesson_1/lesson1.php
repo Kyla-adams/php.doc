c47be184b042de7a554274718f2515a571912122
Kyla-adams <astonadam79@gmail.com>
2020-02-04 11:35:29 +0300

    First php file pushed

---
+++ Lesson_1/lesson1.php
@@ -0,0 +1,36 @@
+<?php
+//Introduction to php
+//PHP is a widely-used, open source scripting language
+//PHP scripts are executed on the server
+//PHP files can contain text, HTML, CSS, Javascript, and PHP CODE
+//PHP code is executed on the server, and the result is returned to the browser as plain HTML
+//PHP files have extension ".php"
+echo "Hello world";
+echo "<h1>Hello world</h1>";
+//echo: print content in browser
+//Hello world: the content to be printed on the browser
+//; : means end of php statement or instruction
+
+//VARIABLES IN PHP
+//a container for storing data add
+
+$myname = "Kyla Adam <br>";
+echo $myname;
+
+$company1 = "Google <br>";
+$company2 = "Oracle <br>";
+$company3 = "Safaricom <br>";
+
+echo $company1;
+echo $company2;
+echo $company3;
+
+
+
+
+
+
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque doloremque eos officia ullam voluptatum! Amet consectetur cupiditate debitis, ducimus esse explicabo inventore libero necessitatibus non pariatur placeat quam sapiente vel?</p>
+
